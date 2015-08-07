201f7d1d58e93c2d84fc3170d827da17e2edefc7
unknown <gareth.rayner@LAPTOP478.ps.local>
2015-08-07 14:46:49 +0100

    Completed NavDrawer functionality.

--- app/src/main/java/com/mycompany/devapp/NewArt.java
+++ app/src/main/java/com/mycompany/devapp/NewArt.java
@@ -1,6 +1,7 @@
 package com.mycompany.devapp;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -8,15 +9,20 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 public class NewArt extends Activity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private DrawerLayout mDrawerLayout;
+    private RelativeLayout DrawerRelative;
     private ActionBarDrawerToggle navDrawToggle;
     private CharSequence navDrawTitle;
     private CharSequence title;
@@ -28,6 +34,7 @@ public class NewArt extends Activity {
         setContentView(R.layout.activity_new_art);
         mRecyclerView = (RecyclerView) findViewById(R.id.newArtDisplay);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.navDraw);
+        articleNav = (ListView) findViewById(R.id.navElem);
         title = navDrawTitle = getTitle();
         navDrawToggle = new ActionBarDrawerToggle(this, mDrawerLayout, new Toolbar(this), R.string.drawer_open, R.string.drawer_close) {
             public void onDrawerClosed(View view) {
@@ -42,9 +49,21 @@ public class NewArt extends Activity {
 
             public void onDrawerOpened(View view) {
                 super.onDrawerOpened(view);
-                
+                getActionBar().setTitle(navDrawTitle);
+                invalidateOptionsMenu();
             }
         };
+
+        mDrawerLayout.setDrawerListener(navDrawToggle);
+
+        try {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        } catch(NullPointerException e) {
+            Log.e("ActionBar Error", "NullPtr");
+        }
+
+        getActionBar().setHomeButtonEnabled(true);
+
         articleNav = (ListView) findViewById(R.id.navElem);
 
         mRecyclerView.setHasFixedSize(true);
@@ -65,5 +84,31 @@ public class NewArt extends Activity {
 
         mAdapter = new ArtAdapter(articles);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        navDrawToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        navDrawToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(navDrawToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 }
