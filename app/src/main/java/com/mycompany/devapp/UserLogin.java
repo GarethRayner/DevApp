df464bdf5b500241c8c956e2f58608aef7f8a69e
unknown <gareth.rayner@LAPTOP478.ps.local>
2015-08-11 17:00:49 +0100

    Implemented Async tasks for SQL, SQL functionality and user access system.

---
+++ app/src/main/java/com/mycompany/devapp/UserLogin.java
@@ -0,0 +1,82 @@
+package com.mycompany.devapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UserLogin extends Activity {
+    private UserSQLHelper userDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+
+        userDB = new UserSQLHelper(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_register) {
+            userService(0);
+            return true;
+        } else if(id == R.id.action_delete) {
+            userService(1);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void userService(int type) {
+        switch(type) {
+            case 0:
+                Intent intent1 = new Intent(this, UserRegister.class);
+                startActivity(intent1);
+                break;
+            case 1:
+                //Intent intent2 = new Intent(this, UserDelete.class);
+                //startActivity(intent2);
+                break;
+        }
+    }
+
+    public void userLogin(View view) {
+        EditText user = (EditText) findViewById(R.id.username);
+        EditText pass = (EditText) findViewById(R.id.password);
+
+        String username = user.getText().toString();
+        String password = pass.getText().toString();
+
+        String result = userDB.getUser(username, password);
+        if(result == null) {
+            Toast toast = Toast.makeText(this, "No user found", Toast.LENGTH_SHORT);
+            toast.show();
+        } else if(result.compareTo(password) == 0) {
+            Intent intent = new Intent(this, HomeScreen.class);
+            startActivity(intent);
+        } else {
+            Toast toast = Toast.makeText(this, "Wrong user/password", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
---
+++ app/src/main/java/com/mycompany/devapp/UserRegister.java
@@ -0,0 +1,56 @@
+package com.mycompany.devapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UserRegister extends Activity {
+    private UserSQLHelper userDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+
+        userDB = new UserSQLHelper(this);
+    }
+
+    public void registerUser(View view) {
+        EditText user = (EditText) findViewById(R.id.username);
+        EditText pass = (EditText) findViewById(R.id.password);
+
+        String username = user.getText().toString();
+        String password = pass.getText().toString();
+
+        InsertUserTask bGTask = new InsertUserTask();
+        bGTask.setDB(userDB);
+        String[] args = new String[2];
+        args[0] = username;
+        args[1] = password;
+        bGTask.execute(args);
+        Toast toast = Toast.makeText(this, "User Created!", Toast.LENGTH_SHORT);
+        toast.show();
+
+        Intent intent = new Intent(this, UserLogin.class);
+        startActivity(intent);
+    }
+
+    private class InsertUserTask extends AsyncTask<String, Void, Void> {
+        private UserSQLHelper data;
+
+        @Override
+        protected Void doInBackground(String... params) {
+            data.addUser(params[0], params[1]);
+            return null;
+        }
+
+        public void setDB(UserSQLHelper db) {
+            data = db;
+        }
+    }
+}
