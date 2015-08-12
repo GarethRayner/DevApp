46a998287846f2fe27ebaac3b42012899b6b3237
unknown <gareth.rayner@LAPTOP478.ps.local>
2015-08-12 11:25:18 +0100

    Implemented full verification functionality.

--- app/src/main/java/com/mycompany/devapp/VerifyFunctionality.java
+++ app/src/main/java/com/mycompany/devapp/VerifyFunctionality.java
@@ -17,41 +17,58 @@ public class VerifyFunctionality extends Activity {
         setContentView(R.layout.activity_verify_functionality);
     }
 
-    public void verifyApp(View view) {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.verify_ic)
-                .setContentTitle("App Verified")
-                .setContentText("The app is working correctly");
-
-        Intent resultIntent = new Intent(this, HomeScreen.class);
-
-        PendingIntent backtrack = PendingIntent.getActivity(
-                this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        mBuilder.setContentIntent(backtrack);
-
-        int notificationId = 001;
-
-        NotificationManager notify = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notify.notify(notificationId, mBuilder.build());
-    }
-
-    public void verifyArticleDuck(View view) {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.verify_ic)
-                        .setContentTitle("Duck Article Verified")
-                        .setContentText("Duck article viewing is working correctly.");
-
+    public void verify(View view) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.verify_ic);
         mBuilder.setAutoCancel(true);
 
-        Intent resultIntent = new Intent(this, ArticleRead.class);
-        resultIntent.putExtra("article", "duck");
-
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(ArticleRead.class);
-        stackBuilder.addNextIntent(resultIntent);
+
+        String tag = view.getTag().toString();
+        switch(tag) {
+            case "duck":
+                mBuilder.setContentTitle("Duck Article Verified");
+                mBuilder.setContentText("Duck article viewing is working correctly.");
+                Intent duckResultIntent = new Intent(this, ArticleRead.class);
+                duckResultIntent.putExtra("article", "duck");
+
+                stackBuilder.addParentStack(ArticleRead.class);
+                stackBuilder.addNextIntent(duckResultIntent);
+                break;
+            case "geese":
+                mBuilder.setContentTitle("Geese Article Verified");
+                mBuilder.setContentText("Geese article viewing is working correctly.");
+                Intent geeseResultIntent = new Intent(this, ArticleRead.class);
+                geeseResultIntent.putExtra("article", "geese");
+
+                stackBuilder.addParentStack(ArticleRead.class);
+                stackBuilder.addNextIntent(geeseResultIntent);
+                break;
+            case "slider":
+                mBuilder.setContentTitle("Article Slider Verified");
+                mBuilder.setContentText("Article Slider is working correctly.");
+                Intent sliderResultIntent = new Intent(this, ArticleSlider.class);
+
+                stackBuilder.addParentStack(ArticleSlider.class);
+                stackBuilder.addNextIntent(sliderResultIntent);
+                break;
+            case "new":
+                mBuilder.setContentTitle("New Article Viewer Verified");
+                mBuilder.setContentText("New article viewing is working correctly.");
+                Intent newResultIntent = new Intent(this, NewArt.class);
+
+                stackBuilder.addParentStack(NewArt.class);
+                stackBuilder.addNextIntent(newResultIntent);
+                break;
+            case "free":
+                mBuilder.setContentTitle("Free Press Verified");
+                mBuilder.setContentText("Free Press entry is working correctly.");
+                Intent resultIntent = new Intent(this, FreePress.class);
+
+                stackBuilder.addParentStack(FreePress.class);
+                stackBuilder.addNextIntent(resultIntent);
+                break;
+        }
 
         PendingIntent backtrack = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
